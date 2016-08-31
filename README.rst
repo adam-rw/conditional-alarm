f86a2fa59697dffe4ca7dc74a95ad151a5f09910
Adam Williams <adam.williams@linux.com>
2016-08-31 07:04:22 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Conditional Alarm
+========================
+
+Wake up alarm app that runs as a daemon and alerts based on specified times subject to optional conditions such as weather conditions.
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='conditional-alarm',
+    version='0.0.1',
+    description='Conditional Alarm Clock',
+    long_description=readme,
+    author='Adam Williams',
+    author_email='adam.williams@linux.com',
+    url='https://github.com/rofl-lol/conditional-alarm',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
